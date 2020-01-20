0b3b9ba8b208b90bdbb2a5e0bd8a491a95fbe5eb
Marco Niederberger <marco.niederberger@gmail.com>
2020-01-20 21:13:40 +0100

    *** first thetvdb

--- app/tv/routes.py
+++ app/tv/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template
+import os, urllib.parse, requests
+
+from flask import current_app, render_template
 
 from app.tv import bp
 
@@ -6,4 +8,11 @@ from app.tv import bp
 @bp.route('/tv/', methods=['GET'])
 @bp.route('/tv/index', methods=['GET'])
 def index():
-    return render_template('tv/index.html', title='TV')
+    headers = {"Authorization": "Bearer {}".format(current_app.config['THETVDB_API_KEY'])}
+    files = os.listdir(current_app.config['TV_IN'])
+    for file in files:
+        query = '{}/search/tv?query={}'.format(current_app.config['THETVDB_API_URL'], urllib.parse.quote(file))
+        print(query)
+        print(requests.get(query, headers=headers).json())
+
+    return render_template('tv/index.html', title='TV', files=files)
