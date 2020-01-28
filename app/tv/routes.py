42ce2adb86a658b285168e39235717fb98448d35
Marco Niederberger <marco.niederberger@adcubum.com>
2020-01-28 21:59:48 +0100

    *** regex used in call

--- app/tv/routes.py
+++ app/tv/routes.py
@@ -1,4 +1,4 @@
-import os, urllib.parse, requests
+import os, urllib.parse, requests, re
 
 from flask import current_app, render_template
 
@@ -12,9 +12,18 @@ def index():
     headers = {"Authorization": "Bearer {}".format(current_app.config['THETVDB_API_KEY'])}
     files = os.listdir(current_app.config['TV_IN'])
     for file in files:
-        
-        query = '{}/search/tv?query={}'.format(current_app.config['THETVDB_API_URL'], urllib.parse.quote(file))
-        print(query)
-        print(requests.get(query, headers=headers).json())
+        m = search_with_regex_for_tv_patterns(file)
+        if m:
+            query = '{}/search/tv?query={}'.format(current_app.config['THETVDB_API_URL'], urllib.parse.quote(m['seriesname']))
+            print(query)
+            print(requests.get(query, headers=headers).json())
 
     return render_template('tv/index.html', title='TV', files=files)
+
+
+def search_with_regex_for_tv_patterns(file):
+    regs = current_app.config['tv_patterns']
+    for reg in regs:
+        m = re.search(reg['pattern'], file)
+        if m:
+            return m.groupdict()
